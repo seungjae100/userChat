dae2e861a143dc8533b5f9106b404ac65b4c8b79
seungjae100 <seungjaebaekmail@gmail.com>
2024-10-08 14:24:43 +0900

    채팅프로젝트 생성

---
+++ src/main/java/com/web/userchat/UserChatApplication.java
@@ -0,0 +1,13 @@
+package com.web.userchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserChatApplication.class, args);
+    }
+
+}
