de8d7c1986ef4efb036d35ec90c3a1186fceffb5
seungjae100 <seungjaebaekmail@gmail.com>
2025-01-16 14:34:23 +0900

    refactoring-#21 : resultMap으로 1:N의 관계를 지정해줌 User, ChatRoom 다수관계 지정 완료2

--- src/main/java/com/web/userchat/model/ChatRoom.java
+++ src/main/java/com/web/userchat/model/ChatRoom.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,4 +14,8 @@ public class ChatRoom {
     private String roomName; // 채팅방 이름
     private int userCount; // 채팅방 인원 수
     private LocalDateTime createdAt; // 채팅방이 만들어진 시간
+
+    /* 1 대 다수의 관계*/
+
+    private List<ChatMessage> chatMessages;
 }
