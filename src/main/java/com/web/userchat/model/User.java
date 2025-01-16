3abadf9edafb533069fc819db8a3e65003d91939
seungjae100 <seungjaebaekmail@gmail.com>
2025-01-16 14:30:18 +0900

    refactoring-#21 : resultMap으로 1:N의 관계를 지정해줌 User, ChatRoom 다수관계 지정 완료

--- src/main/java/com/web/userchat/model/User.java
+++ src/main/java/com/web/userchat/model/User.java
@@ -4,6 +4,8 @@ package com.web.userchat.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class User {
@@ -15,4 +17,8 @@ public class User {
     private String email; // 사용자 이메일
 
     private String password; // 사용자 비밀번호
+
+    /* 1 대 다수의 관계*/
+    private List<ChatRoom> chatRooms;
+    private List<ChatMessage> chatMessages;
 }
